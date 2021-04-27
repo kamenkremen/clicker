8b1bc4eb12fe5c49e3a0473a611e20ebdd225fbe
kamenkremen <62587838+kamenkremen@users.noreply.github.com>
2021-04-27 14:29:32 +0700

    Версия для heroku

--- main-restful.py
+++ main-restful.py
@@ -6,4 +6,5 @@ from website import create_app, create_session
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
